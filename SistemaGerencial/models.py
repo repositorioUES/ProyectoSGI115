783f6dff42292a42caf057b6266ba8115baeb691
Your Name <you@example.com>
2022-05-21 18:07:14 -0600

    Login

--- SistemaGerencial/models.py
+++ SistemaGerencial/models.py
@@ -1,3 +1,7 @@
 from django.db import models
-
+from django.contrib.auth.models import AbstractUser
 # Create your models here.
+class User(AbstractUser):
+    is_administador = models.BooleanField('Administrador', default=False)
+    is_estrategico = models.BooleanField('Estrategico', default=False)
+    is_tactico = models.BooleanField('Tactico', default=False)
